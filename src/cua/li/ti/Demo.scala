cf85d8f7986c8169bef9cde159c90c442169807e
Alain Bearez <Alain@cuali.com.br>
2012-05-19 09:54:27 -0300

    first custom layout ShiftingStackPane

--- src/cua/li/ti/Demo.scala
+++ src/cua/li/ti/Demo.scala
@@ -1,6 +1,7 @@
 package cua.li.ti
 
 import cua.li.ti.scene.gadget.HexagonalColorPicker
+import cua.li.ti.scene.layout.ShiftingStackPane
 
 import scalafx.Includes._
 import scalafx.application.JFXApp
@@ -12,38 +13,44 @@ import scalafx.scene.paint.Color
 import scalafx.scene.shape.Rectangle
 import scalafx.stage.Stage
 
-
 /**
  * @author A@cua.li
  */
 object Demo extends JFXApp {
-  val RADIUS = 230
-  val PICKER =
-    new HexagonalColorPicker {
-      centerX = RADIUS
-      centerY = Math.sqrt(3) * RADIUS / 2
-      onClose = (color: Color, hasChanged: Boolean) => {
-        if (hasChanged) { stage.scene.value.fill = color }
-      }
-    }
-  stage = new Stage {
-    title = "Demo"
-    scene = new Scene(2 * RADIUS, Math.sqrt(3) * RADIUS) {
-      content = new StackPane {
-        content = Seq(
-          new Button {
-            text = "Show Hexagonal Color Picker"
-            onMouseClicked = (me: MouseEvent) => {
-              PICKER.radius = RADIUS
-              PICKER.original = Color.BLACK
-              PICKER.managed = ! PICKER.managed.get
-              PICKER.visible = ! PICKER.visible.get
+    val RADIUS = 230
+    val PICKER =
+        new HexagonalColorPicker {
+            centerX = RADIUS
+            centerY = Math.sqrt(3) * RADIUS / 2
+            onClose = (color : Color, hasChanged : Boolean) => {
+                if (hasChanged) { stage.scene.value.fill = color }
             }
-          },
-          PICKER
-          )
-      }
-      fill = Color.BLANCHEDALMOND
+        }
+    stage = new Stage {
+        title = "Demo"
+        scene = new Scene(2 * RADIUS, Math.sqrt(3) * RADIUS) {
+            content = new StackPane {
+                content = Seq(
+                    new ShiftingStackPane(5, 5) {
+                        content = Seq(
+                            new Rectangle { width = 300; height = 100; fill = Color.DARKORANGE },
+                            new Rectangle { width = 300; height = 100; fill = Color.DARKTURQUOISE },
+                            new Rectangle { width = 300; height = 100; fill = Color.DARKOLIVEGREEN }
+                        )
+                    },
+                    new Button {
+                        text = "Show Hexagonal Color Picker"
+                        onMouseClicked = (me : MouseEvent) => {
+                            PICKER.radius = RADIUS
+                            PICKER.original = Color.BLACK
+                            PICKER.managed = !PICKER.managed.get
+                            PICKER.visible = !PICKER.visible.get
+                        }
+                    },
+                    PICKER
+                )
+            }
+            fill = Color.BLANCHEDALMOND
+        }
     }
-  }
 }
---
+++ src/cua/li/ti/scene/layout/ShiftingStackPane.scala
@@ -0,0 +1,64 @@
+package cua.li.ti.scene.layout
+
+import javafx.collections.ListChangeListener
+import javafx.geometry.HPos
+import javafx.geometry.VPos
+import javafx.{ scene => jfxs }
+import jfxs.{ layout => jfxsl }
+
+import scalafx.Includes._
+import scalafx.scene.Node
+import scalafx.scene.layout.StackPane
+
+/**
+ * In its current implementation this class assumes all content nodes are the same size.
+ * It does NOT honor the <code>nodeHPos</code> and <code>nodeVPos</code> attributes.
+ * @author A@cua.li
+ */
+
+class ShiftingStackPane(override val delegate :jfxsl.StackPane) extends StackPane(delegate) {
+	def this(shiftX :Double, shiftY :Double) = this(new ShiftingStackPane.ExtendedStackPane(shiftX, shiftY))
+}
+object ShiftingStackPane {
+    private[layout] class ExtendedStackPane(shiftX :Double, shiftY :Double)  extends jfxsl.StackPane {
+	    private[this] var preferredHeight :Double = 0
+	    private[this] var dirtyPrefHeight = true
+	    override def computePrefHeight(width :Double) = {
+	        if (dirtyPrefHeight) {
+	            dirtyPrefHeight = false
+		        val sizeOfContent = (super.getManagedChildren().size) - 1
+		        preferredHeight = super.computePrefHeight(width) + shiftY * sizeOfContent
+	        }
+	        preferredHeight
+	    }
+	    private[this] var preferredWidth :Double = 0
+	    private[this] var dirtyPrefWidth = true
+	    override def computePrefWidth(height :Double) = {
+	        if (dirtyPrefWidth) {
+	            dirtyPrefWidth = false
+		        val sizeOfContent = (super.getManagedChildren().size) - 1
+		        preferredWidth = super.computePrefWidth(height) + shiftX * sizeOfContent
+	        }
+	        preferredWidth
+	    }
+	    override def layoutChildren() = {
+	        val sizeOfContent = super.getManagedChildren().size
+	        if (0 < sizeOfContent) {
+	        	val managedContent = super.getManagedChildren().iterator()
+	            var nodeShift = sizeOfContent
+		        while (managedContent.hasNext) {
+		            layoutInArea(managedContent.next, shiftX * nodeShift, shiftY * nodeShift, 
+		                    preferredWidth, preferredHeight, 0, HPos.RIGHT, VPos.BOTTOM)
+		            nodeShift -= 1
+		        }
+		    }
+	    }
+	    private[this] val contentChangeListener =  new ListChangeListener[jfxs.Node] {
+	        override def onChanged (change :ListChangeListener.Change[_ <:jfxs.Node]) = {
+	            dirtyPrefHeight = true
+	            dirtyPrefWidth = true
+	        }
+	    }
+	    getChildren.addListener(contentChangeListener)
+    }
+}
